91fb39544c325b278d434a2a9218de674b9a2a0e
Nezih <nezih.tartilaci@gavth.com>
2020-08-26 23:17:37 +0300

    Mongo DB setup completed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,55 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const articleRoutes = require('./api/routes/articles');
 const categoryRoutes = require('./api/routes/categories');
 
+mongoose.connect('mongodb+srv://'
+    + process.env.MONGO_ATLAS_USER + ':'
+    + process.env.MONGO_ATLAS_PW + '@cluster0.dxbex.mongodb.net/'
+    + process.env.MONGO_ATLAS_DB + '?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Http request logger
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Avoid cors errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Routes which handle api requests
 app.use('/articles', articleRoutes);
 app.use('/categories', categoryRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+            statusCode: error.status
+        }
+    })
+});
 
 module.exports = app;

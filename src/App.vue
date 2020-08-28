ffb3d8c490e2a1032ce6f56c1e5b2dc1e995339f
Nezih <nezih.tartilaci@gavth.com>
2020-08-28 22:35:25 +0300

    First blog UI development

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <AuthorInfo />
+        <Articles />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import AuthorInfo from "./components/AuthorInfo.vue";
+    import Articles from "./components/Articles.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: "App",
+        components: {
+            AuthorInfo,
+            Articles
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
